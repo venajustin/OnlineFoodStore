7d621f8be6e65f4fe7ee3618dc3b8a2b27422736
j3narciso <j3narcisojp@gmail.com>
2023-10-27 12:43:09 -0700

    Update search.php

--- templates/search.php
+++ templates/search.php
@@ -10,17 +10,34 @@
 
     // check connection 
     $search = $_POST["search"];
+    $searchq = "SELECT * FROM items WHERE item_description LIKE '%$search%'";
+    $itemS = mysqli_query($conn,$searchq);
     if (!$conn ) { 
         die ("Connection failed: " . mysqli_connect_error());
     } 
     else {
-        echo $search;
+        if ($result = $mysqli->query($query)) {
+
+            /* fetch associative array */
+            while ($row = $result->fetch_assoc()) {
+                $field1name = $row["col1"];
+                $field2name = $row["col2"];
+                $field3name = $row["col3"];
+                $field4name = $row["col4"];
+                $field5name = $row["col5"];
+                echo $field2name;
+                echo "<br>";
+            }
+        
+            /* free result set */
+            $result->free();
+        }
         exit();
     
         }
     
 
-    //search call (see if the exact word is contained in the database)
+    // search call (see if the exact word is contained in the database)
     
 
 ?>

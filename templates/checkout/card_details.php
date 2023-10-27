98239d8d8b72154c9b9a1d2d6abfaca1dc97a280
Matthew Tran <matthewtran0122@gmail.com>
2023-10-27 13:35:30 -0700

    Update card_details.php

--- templates/checkout/card_details.php
+++ templates/checkout/card_details.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
     session_start();
-    if (!isset($_SESSION["username"]) {
+    if (!isset($_SESSION["username"])) {
         header('Location: '.$uri.'/OnlineFoodStore/templates/login.php');
         exit;
         }

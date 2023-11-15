24646ce13d27868f5f0812a1bf54efd5acf2439f
Matthew Tran <matthewtran0122@gmail.com>
2023-11-14 21:27:44 -0800

    updatable items stock

---
+++ routes/update_item.php
@@ -0,0 +1,39 @@
+
+<?php
+
+    session_start();
+    unset($_SESSION["address_error"]);
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    require "../../credentials.php";
+
+    // create connection 
+    $conn = mysqli_connect($hostname, $dbuser, $dbpass, $dbname);
+    // check connection 
+    if (!$conn) { 
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $item_id = $_POST["item_id"];
+        $userNumber = $_POST["userNumber"];
+    
+        $sql = "UPDATE items SET inv_count = inv_count + $userNumber WHERE item_id = $item_id";
+    
+        if ($conn->query($sql) === TRUE) {
+            echo "Stock updated successfully";
+        } else {
+            echo "Error updating stock: " . $conn->error;
+        }
+    }
+    
+    header("Location: ../templates/managerpage.php");
+    exit();
+?>

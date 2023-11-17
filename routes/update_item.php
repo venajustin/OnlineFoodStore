21ef9c3767828ee49d21040e3fda64b942682071
Matthew Tran <matthewtran0122@gmail.com>
2023-11-17 14:35:06 -0800

    summary page made

--- routes/update_item.php
+++ routes/update_item.php
@@ -22,7 +22,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $item_id = $_POST["item_id"];
+        $item_id = $_POST["iid"];
         $userNumber = $_POST["userNumber"];
         $nameChange = $_POST["newName"];
         $newPrice = $_POST["userPrice"];
